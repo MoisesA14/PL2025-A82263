188b1853c04e4d882a90f7496b5c49cd0b687ac6
Moisés Antunes <36673656+MoisesA14@users.noreply.github.com>
2025-02-13 23:11:08 +0000

    Create tpc1.py

---
+++ TPC1/tpc1.py
@@ -0,0 +1,27 @@
+import sys
+
+def soma_on_off(input_file : str) -> None:
+    with open(input_file, "r") as file:
+        texto = file.read().lower()
+
+    total = 0
+    i = 0
+    ligado = True
+
+    while i < len(texto):
+        if texto[i] == 'o':  
+            if texto[i: i + 2] == 'on':  
+                ligado = True
+                i += 1
+            elif texto[i: i + 3] == 'off':  
+                ligado = False
+                i += 3
+        elif texto[i] == '=':
+            print(total)
+        elif ligado and texto[i].isdigit():
+            total += int(texto[i])
+        i += 1
+
+if __name__ == "__main__":
+    input_file = sys.argv[1]
+    soma_on_off(input_file)
